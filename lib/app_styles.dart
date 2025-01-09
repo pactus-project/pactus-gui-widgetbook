4ef24576e93441595ced953a7700dda6db0fafd4
Esmaeil Ahmadipour <46590079+esmaeil-ahmadipour@users.noreply.github.com>
2025-01-09 18:50:42 +0330

    chore: add/create a barrel exports file for app styles (#9)
    Co-authored-by: pouria.moradi <P@M14030619>

---
+++ lib/app_styles.dart
@@ -0,0 +1,5 @@
+export 'package:pactus_gui_widgetbook/src/core/pallets/on_surface_pallet.dart';
+export 'package:pactus_gui_widgetbook/src/core/pallets/surface_pallet.dart';
+export 'package:pactus_gui_widgetbook/src/core/text_styles/inter_text_styles.dart';
+export 'package:pactus_gui_widgetbook/src/core/theme/app_theme.dart';
+export 'package:pactus_gui_widgetbook/src/core/theme/app_theme_data.dart';
