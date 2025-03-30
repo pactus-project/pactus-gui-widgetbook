990c81e1229a49fba4c9e4a37cc9da23badb67ef
PouriaMoradi021 <pooriya.mrd1378@gmail.com>
2025-03-31 00:32:43 +0330

    feat: impl `AdaptiveTextButton` widget & it`s use case - add/create `AdaptiveTextButton` - add/create `adaptiveTextButtonUseCase` - update mai.directories file

---
+++ lib/src/features/widgets/buttons/adaptive_text_button/adaptive_text_button.dart
@@ -0,0 +1,156 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:pactus_gui_widgetbook/app_styles.dart';
+import 'package:pactus_gui_widgetbook/src/core/enum/padding_size_enum.dart';
+import 'package:pactus_gui_widgetbook/src/core/enum/request_state_enum.dart';
+import 'package:pactus_gui_widgetbook/src/features/widgets/buttons/core/enums/button_type_enum.dart';
+import 'package:pactus_gui_widgetbook/src/features/widgets/buttons/core/common/widgets/adaptive_button_content_widget.dart';
+
+class AdaptiveTextButton extends StatelessWidget {
+  const AdaptiveTextButton({
+    super.key,
+    required this.requestState,
+    this.onPressed,
+    this.suffixIcon,
+    this.prefixIcon,
+    this.title,
+    required this.buttonType,
+    this.baseIcon,
+    required this.paddingSize,
+  });
+
+  final RequestStateEnum requestState;
+  final Function()? onPressed;
+  final IconData? suffixIcon;
+  final IconData? prefixIcon;
+  final String? title;
+  final ButtonTypeEnum buttonType;
+  final IconData? baseIcon;
+  final PaddingSizeEnum paddingSize;
+
+  // Factory methods to create buttons with specific button types
+  factory AdaptiveTextButton.createTitleOnly({
+    required RequestStateEnum requestState,
+    Function()? onPressed,
+    String? title,
+    PaddingSizeEnum paddingSize = PaddingSizeEnum.medium,
+  }) {
+    return AdaptiveTextButton(
+      requestState: requestState,
+      onPressed: onPressed,
+      title: title,
+      paddingSize: paddingSize,
+      buttonType: ButtonTypeEnum.titleOnly,
+    );
+  }
+
+  factory AdaptiveTextButton.createIconAndTitle({
+    required RequestStateEnum requestState,
+    Function()? onPressed,
+    String? title,
+    PaddingSizeEnum paddingSize = PaddingSizeEnum.medium,
+    IconData? prefixIcon,
+  }) {
+    return AdaptiveTextButton(
+      requestState: requestState,
+      onPressed: onPressed,
+      title: title,
+      paddingSize: paddingSize,
+      buttonType: ButtonTypeEnum.iconAndTitle,
+      prefixIcon: prefixIcon,
+    );
+  }
+
+  factory AdaptiveTextButton.createTitleAndIcon({
+    required RequestStateEnum requestState,
+    Function()? onPressed,
+    String? title,
+    PaddingSizeEnum paddingSize = PaddingSizeEnum.medium,
+    IconData? suffixIcon,
+  }) {
+    return AdaptiveTextButton(
+      requestState: requestState,
+      onPressed: onPressed,
+      title: title,
+      paddingSize: paddingSize,
+      buttonType: ButtonTypeEnum.titleAndIcon,
+      suffixIcon: suffixIcon,
+    );
+  }
+
+  factory AdaptiveTextButton.createIconTitleAndIcon({
+    required RequestStateEnum requestState,
+    Function()? onPressed,
+    String? title,
+    PaddingSizeEnum paddingSize = PaddingSizeEnum.medium,
+    IconData? prefixIcon,
+    IconData? suffixIcon,
+  }) {
+    return AdaptiveTextButton(
+      requestState: requestState,
+      onPressed: onPressed,
+      title: title,
+      paddingSize: paddingSize,
+      buttonType: ButtonTypeEnum.iconTitleAndIcon,
+      prefixIcon: prefixIcon,
+      suffixIcon: suffixIcon,
+    );
+  }
+
+  factory AdaptiveTextButton.createIconOnly({
+    required RequestStateEnum requestState,
+    Function()? onPressed,
+    PaddingSizeEnum paddingSize = PaddingSizeEnum.medium,
+    IconData? baseIcon,
+  }) {
+    return AdaptiveTextButton(
+      requestState: requestState,
+      onPressed: onPressed,
+      paddingSize: paddingSize,
+      buttonType: ButtonTypeEnum.iconOnly,
+      baseIcon: baseIcon,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: Button(
+        style: ButtonStyle(
+          padding: WidgetStateProperty.all(EdgeInsets.symmetric(horizontal: paddingSize.horizontalSize)),
+          backgroundColor: WidgetStateProperty.all(Colors.transparent),
+          foregroundColor: WidgetStateProperty.resolveWith((states) {
+            if (states.isPressed) {
+              return AppTheme.of(context).accentColor.darkest;
+            }
+            if (states.isHovered) {
+              return AppTheme.of(context).accentColor.darker;
+            }
+            if (states.isDisabled) {
+              return AppTheme.of(context).inactiveColor;
+            }
+              return AppTheme.of(context).accentColor;
+          }),
+          elevation: WidgetStateProperty.all(0),
+          shape: WidgetStateProperty.all(const RoundedRectangleBorder(
+            side: BorderSide.none,
+          )),
+        ),
+        onPressed: (requestState == RequestStateEnum.loading) ? null : onPressed,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: paddingSize.horizontalSize),
+          child: AdaptiveButtonContent(
+            requestState: requestState,
+            suffixIcon: suffixIcon,
+            prefixIcon: prefixIcon,
+            title: title,
+            loadingDotColor: AppTheme.of(context).accentColor.lightest,
+            buttonType: buttonType,
+            icon: baseIcon,
+            paddingSize: paddingSize,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/features/widgets/buttons/adaptive_text_button/adaptive_text_button_usecase.dart
@@ -0,0 +1,89 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:pactus_gui_widgetbook/src/core/enum/padding_size_enum.dart';
+import 'package:pactus_gui_widgetbook/src/core/enum/request_state_enum.dart';
+import 'package:pactus_gui_widgetbook/src/features/widgets/buttons/adaptive_text_button/adaptive_text_button.dart';
+import 'package:pactus_gui_widgetbook/src/features/widgets/buttons/core/enums/button_type_enum.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart';
+
+@UseCase(
+  name: 'Adaptive Text Button with Knobs',
+  type: AdaptiveTextButton,
+)
+Widget adaptiveTextButtonUseCase(BuildContext context) {
+  final requestState = context.knobs.list<RequestStateEnum>(
+    label: 'Request State',
+    options: RequestStateEnum.values,
+    initialOption: RequestStateEnum.loaded,
+  );
+
+  final isDisabled = context.knobs.boolean(
+    label: 'Disable Button',
+    initialValue: false,
+  );
+
+  final text = context.knobs.string(
+    label: 'Button Text',
+    initialValue: 'Click Me',
+  );
+
+  final paddingSize = context.knobs.list<PaddingSizeEnum>(
+    label: 'Padding Size',
+    options: PaddingSizeEnum.values,
+    initialOption: PaddingSizeEnum.min,
+  );
+  final buttonType = context.knobs.list<ButtonTypeEnum>(
+    label: 'Button Type',
+    options: ButtonTypeEnum.values,
+    initialOption: ButtonTypeEnum.titleOnly,
+  );
+
+  final prefixIcon = context.knobs.listOrNull<IconData?>(
+    label: 'Prefix Icon',
+    options: [FluentIcons.add, FluentIcons.settings],
+    initialOption: FluentIcons.add,
+  );
+
+  final suffixIcon = context.knobs.listOrNull<IconData?>(
+    label: 'Suffix Icon',
+    options: [FluentIcons.chevron_right, FluentIcons.search],
+    initialOption: FluentIcons.chevron_right,
+  );
+
+  final baseIcon = context.knobs.listOrNull<IconData?>(
+    label: 'Base Icon',
+    options: [FluentIcons.search, FluentIcons.add],
+    initialOption: FluentIcons.search,
+  );
+
+  return IntrinsicWidth(
+    child: SizedBox(
+      height: 40,
+      child: AdaptiveTextButton(
+        title: buttonType == ButtonTypeEnum.titleOnly ||
+            buttonType == ButtonTypeEnum.iconAndTitle ||
+            buttonType == ButtonTypeEnum.titleAndIcon ||
+            buttonType == ButtonTypeEnum.iconTitleAndIcon
+            ? text
+            : null,
+        requestState: requestState,
+        buttonType: buttonType,
+        prefixIcon: buttonType == ButtonTypeEnum.iconAndTitle ||
+            buttonType == ButtonTypeEnum.iconTitleAndIcon
+            ? prefixIcon
+            : null,
+        suffixIcon: buttonType == ButtonTypeEnum.titleAndIcon ||
+            buttonType == ButtonTypeEnum.iconTitleAndIcon
+            ? suffixIcon
+            : null,
+        baseIcon: buttonType == ButtonTypeEnum.iconOnly ? baseIcon : null,
+        onPressed: (requestState == RequestStateEnum.loading || isDisabled)
+            ? null
+            : () {
+          debugPrint('Adaptive Text Button Pressed');
+        },
+        paddingSize: paddingSize,
+      ),
+    ),
+  );
+}
